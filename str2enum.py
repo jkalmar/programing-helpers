ca98f5b91f16ca546b01e153c56fd412b906baf3
jkalmar <janko.kalmar@gmail.com>
2019-05-12 20:58:15 +0200

    adjusted formating of --help

--- str2enum.py
+++ str2enum.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+import textwrap as _textwrap
 
+
+class LineWrapRawTextHelpFormatter(argparse.RawDescriptionHelpFormatter):
+    def _split_lines(self, text, width):
+        text = self._whitespace_matcher.sub(' ', text).strip()
+        return _textwrap.wrap(text, 120)
 
 
 def generateIf():
@@ -13,14 +19,20 @@ def generateEnum():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("str2enum",
-        description="""Simple script to generate C/C++ enum from values. It can generate
-        just the enum or also helper function to convert string to the enum""")
+    parser = argparse.ArgumentParser(prog="str2enum",
+                                     description=("Simple script to generate C/C++ enum from values. It can generate\n"
+                                                  "just the enum or also helper function to convert string to enum"),
+                                     formatter_class=LineWrapRawTextHelpFormatter)
 
-    parser.add_argument("--only-enum", action="store_true", help="Generate only enum")
-    parser.add_argument("--enummode", default="plain", const="plain", nargs="?", choices=["plain", "class"], help="Generate plain enum or enum class(c++11), default=%(default)s")
-    parser.add_argument("--strmode", default="if", const="if", nargs="?", choices=["if", "strcmp", "operator", "map"], help="Set str -> enum conversion, default=%(default)s" )
-    parser.add_argument("--values", nargs="+")
+    parser.add_argument("--only-enum", action="store_true",
+                        help="generate only enum")
+    parser.add_argument("--enummode", default="plain", const="plain", nargs="?", choices=["plain", "class"],
+                        help="generate plain enum or enum class(c++11), default=%(default)s")
+    parser.add_argument("--strmode", default="if", const="if", nargs="?",
+                        choices=["if", "strcmp", "operator", "map"],
+                        help="set str -> enum conversion, default=%(default)s")
+    parser.add_argument("--values", nargs="+",
+                        help="strings from which enum will be created")
 
     parsed = parser.parse_args()
 
@@ -28,4 +40,3 @@ if __name__ == "__main__":
     print(parsed.enummode)
     print(parsed.strmode)
     print(parsed.values)
-
